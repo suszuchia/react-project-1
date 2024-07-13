9b54b8372efc14d84771b12300f92de75b5c4042
Szuchia Su <suszuchia@gmail.com>
2024-07-13 16:57:07 +0100

    fourth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      {/* You can add any footer content here if needed */}
+      <div className="footer-content">
+        <p>© 2024 My Project Website. All rights reserved.</p>
+        <div className="footer-icons">
+          <i className="footer-icon fab fa-facebook"></i>
+          <i className="footer-icon fab fa-twitter"></i>
+          <i className="footer-icon fab fa-instagram"></i>
+        </div>
+      </div>
     </footer>
   );
 };
